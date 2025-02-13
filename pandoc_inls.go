24143011da5ae1cd4a25bfeb1b8f097cedb3f34c
Fred Baube <fbaube@iki.fi>
2025-02-13 17:44:32 +0200

    Bye

--- pandoc_inls.go
+++
@@ -1,6 +0,0 @@
-package dsmnd
-
-// https://hackage.haskell.org/package/pandoc-types-1.23.1/docs/Text-Pandoc-Definition.html
-
-// https://hackage.haskell.org/package/pandoc-types-1.10/docs/src/Text-Pandoc-Definition.html
-
